4bc9030a74dba6304bf63e5fc10e85775db51adf
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-02-19 18:18:38 +0530

    chore: Add favicon, and logo to navbar

--- src/components/Legend/Legend.js
+++ src/components/Legend/Legend.js
@@ -2,37 +2,43 @@ import React from "react";
 import "./Legend.css";
 
 const Legend = () => {
-  const legend = [
-    { name: "Source Node", nodeClass: "node-source" },
-    { name: "Target Node", nodeClass: "node-target" },
-    { name: "Wall Node", nodeClass: "node-wall" },
-    { name: "Unvisited Node", nodeClass: "node" },
-    { name: "Visited Node", nodeClass: "node-visited" },
-    {
-      name: "Shortest Path",
-      nodeClass: "node-shortest-path node-legend-final",
-    },
-  ];
+	const legend = [
+		{ name: "Source Node", nodeClass: "node-source" },
+		{ name: "Target Node", nodeClass: "node-target" },
+		{ name: "Wall Node", nodeClass: "node-wall" },
+		{ name: "Unvisited Node", nodeClass: "node" },
+		{ name: "Visited Node", nodeClass: "node-visited" },
+		{
+			name: "Shortest Path",
+			nodeClass: "node-shortest-path node-legend-final",
+		},
+	];
 
-  return (
-    <div className="container legend my-2">
-      {legend.map((legendItem, idx) => {
-        const { name, nodeClass } = legendItem;
-        return (
-          <button key={idx} className="mr-2 my-1" style={{ cursor: "default" }}>
-            <div className={`node node-legend ${nodeClass}`}></div> {name}
-          </button>
-        );
-      })}
-      <div
-        className="mt-1 font-weight-bold text-danger"
-        style={{ cursor: "default" }}
-      >
-        Note: View on the maximized window of a computer screen for better
-        experience.
-      </div>
-    </div>
-  );
+	return (
+		<div className="container legend my-2">
+			{legend.map((legendItem, idx) => {
+				const { name, nodeClass } = legendItem;
+
+				return (
+					<button
+						key={idx}
+						className="mr-2 my-1"
+						style={{ cursor: "default" }}
+					>
+						<div className={`node node-legend ${nodeClass}`}></div>{" "}
+						{name}
+					</button>
+				);
+			})}
+			<div
+				className="mt-1 font-weight-bold text-danger"
+				style={{ cursor: "default" }}
+			>
+				Note: View on the maximized window of a computer screen for
+				better experience.
+			</div>
+		</div>
+	);
 };
 
 export default Legend;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
+import logo from "../../logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Navbar.css";
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar bg-primary text-light sticky-top">
-          <div className="navbar-brand font-weight-bold">
-            Algorithm Visualizer
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
+const Navbar = () => {
+	return (
+		<div>
+			<nav className="navbar text-light sticky-top">
+				<div className="navbar-brand font-weight-bold">
+					<img src={logo} alt="Logo" className="logo mr-3" />
+					Algorithm Visualizer
+				</div>
+			</nav>
+		</div>
+	);
+};
+
+export default Navbar;
